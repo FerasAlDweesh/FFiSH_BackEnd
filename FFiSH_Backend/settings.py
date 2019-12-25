45d124835caf20e524b4be7d0ebaeec0d99f63ec
Feras AlDweesh <ferasaldweesh@gmail.com>
2019-12-25 17:49:45 +0300

    fixed indentation error

--- FFiSH_Backend/settings.py
+++ FFiSH_Backend/settings.py
@@ -74,24 +74,25 @@ WSGI_APPLICATION = 'FFiSH_Backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
 if DEBUG:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 else:
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': '7f2d45ec802147427364bf8f8d95425f',
-        'HOST': 'localhost',
-        'PORT': '',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'django',
+            'USER': 'django',
+            'PASSWORD': '7f2d45ec802147427364bf8f8d95425f',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
