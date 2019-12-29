947c6bea43439dbdb6d31c6ffcc1a9708bf9dc87
Feras AlDweesh <ferasaldweesh@gmail.com>
2019-12-29 19:54:21 +0300

    POINTS WORK!

--- api/models.py
+++ api/models.py
@@ -16,11 +16,14 @@ class Card(models.Model):
 	vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='vendors')
 
 	def __str__(self):
-		return self.user.username
+		return "%s %s" % (self.vendor, self.user.username)
 
 class Point(models.Model):
 	date = models.DateTimeField(auto_now_add=True)
 	card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name='points')
+
+	def __str__(self):
+		return "%s %s" % (self.card.vendor, self.card.user.username)
 	
 class Reward(models.Model):
 	date = models.DateTimeField(auto_now_add=True)
